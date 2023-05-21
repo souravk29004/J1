e958b61f96ae8650e6aafd9bcb5c63ec4640c811
Souravk29004 <88694528+Souravk29004@users.noreply.github.com>
2023-05-21 17:14:20 +0530

    first commit

--- un.java
+++ un.java
@@ -1,28 +1,15 @@
-import java.util.Scanner;
-
 public class un {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        //expression= operands + operators
+        //operands= variables, quantity, valune numbers
+        //operators= +,=,%,-./
 
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
+        double wedr = 10;
+        
+        wedr =(Double) wedr/3;
 
-        System.out.println("What is your age?");
-        Integer age = scanner.nextInt();
-        scanner.nextLine();//to clear \n from scanner
-
-        System.out.println("What is your favourite food?");
-        String food = scanner.nextLine();
-
-        System.out.println("hello "+ name);
-        System.out.println("Your are "+ age+ " year old.");
-        System.out.println("your favourite food is "+ food);
-
-    
-
-
- 
+        System.out.println(wedr);
     
     }
     
