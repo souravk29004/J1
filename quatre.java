c13e00e35024516c50ba8eb39728112666cec2e4
Souravk29004 <88694528+Souravk29004@users.noreply.github.com>
2023-05-21 17:36:17 +0530

    first commit

---
+++ quatre.java
@@ -0,0 +1,16 @@
+public class quatre {
+    public static void main(String[] args) {
+
+        //expression= operands + operators
+        //operands= variables, quantity, valune numbers
+        //operators= +,=,%,-./
+
+        double wedr = 10;
+
+        wedr =(Double) wedr/3;
+
+        System.out.println(wedr);
+
+    }
+
+}
