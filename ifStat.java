1994ba7dffd1cf7ab222dc3c22bb267766afb008
Souravk29004 <88694528+Souravk29004@users.noreply.github.com>
2023-05-24 00:53:14 +0530

    first commit

---
+++ ifStat.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class ifStat {
+
+    public static void main(String[] args) {
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        
+
+
+        
+        if (age>=21){
+
+            JOptionPane.showMessageDialog(null, "Allowed");
+        }
+
+        else if(age>=18){
+           
+            JOptionPane.showMessageDialog(null, "Allowed but no alcohol");
+
+
+        }
+
+
+        else{
+
+            JOptionPane.showMessageDialog(null, "Not allowed");
+        }
+
+
+
+    }
+
+
+    
+}
