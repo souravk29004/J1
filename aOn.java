8d9f2f072aa824b6cb13003cd2e999fd8f3a97af
Souravk29004 <88694528+Souravk29004@users.noreply.github.com>
2023-06-01 23:30:33 +0530

    first commit

--- aOn.java
+++ aOn.java
@@ -3,7 +3,7 @@ public class aOn{
     public static void main(String[] args) {
         int temp=25;
     if (temp>30) {
-        System.out.println("It is hot outside");
+        System.out.println("It is hot outside.");
     }
     else if (temp>=20 && temp<=30){
         System.out.println("It is warm outside");
