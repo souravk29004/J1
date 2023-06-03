f130614fb8a3ec5383a7cc1efc03b8a0f0435d22
Sourav Kumar <134013290+souravk29004@users.noreply.github.com>
2023-06-03 19:52:45 +0530

    Update wLop.java

--- wLop.java
+++ wLop.java
@@ -15,7 +15,7 @@ public class wLop {
 			System.out.print("Enter your name: ");
 			name = scanner.nextLine();
 		}
-		System.out.println("Welcome "+name);
+		System.out.println("Hey "+name);
 		
 	}
 }
