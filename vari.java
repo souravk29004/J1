fc644e62367d754f682844710384490b9d8e82c9
Sourav Kumar <souravk29004@gmail.com>
2023-06-16 22:26:25 +0530

    Update vari.java

--- vari.java
+++ vari.java
@@ -3,7 +3,7 @@ public class vari {
 	public static void main(String[] args){
 		
 		int x = 1234;
-		double y = 3.14;
+		double y = 3.141;
 		boolean z = true;
 		char symbol = '@';
 		String name = "Bro";
